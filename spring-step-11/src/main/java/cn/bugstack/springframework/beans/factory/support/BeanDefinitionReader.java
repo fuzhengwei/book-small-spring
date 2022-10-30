60502f634d074006cd88a8a67a3dcc6e996028c7
小傅哥 <184172133@qq.com>
2022-10-30 16:26:38 +0800

    小傅哥，feat：第11章　基于JDK、Cglib实现AOP切面

---
+++ spring-step-11/src/main/java/cn/bugstack/springframework/beans/factory/support/BeanDefinitionReader.java
@@ -0,0 +1,27 @@
+package cn.bugstack.springframework.beans.factory.support;
+
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.core.io.Resource;
+import cn.bugstack.springframework.core.io.ResourceLoader;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description Simple interface for bean definition readers.
+ * @date 2022/3/9
+ * @github https://github.com/fuzhengwei
+ * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public interface BeanDefinitionReader {
+
+    BeanDefinitionRegistry getRegistry();
+
+    ResourceLoader getResourceLoader();
+
+    void loadBeanDefinitions(Resource resource) throws BeansException;
+
+    void loadBeanDefinitions(Resource... resources) throws BeansException;
+
+    void loadBeanDefinitions(String location) throws BeansException;
+
+    void loadBeanDefinitions(String... locations) throws BeansException;
+}
---
+++ spring-step-11/src/main/java/cn/bugstack/springframework/core/io/FileSystemResource.java
@@ -0,0 +1,40 @@
+package cn.bugstack.springframework.core.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 文件资源
+ * @date 2022/3/9
+ * @github https://github.com/fuzhengwei
+ * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class FileSystemResource implements Resource {
+
+    private final File file;
+
+    private final String path;
+
+    public FileSystemResource(File file) {
+        this.file = file;
+        this.path = file.getPath();
+    }
+
+    public FileSystemResource(String path) {
+        this.file = new File(path);
+        this.path = path;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new FileInputStream(this.file);
+    }
+
+    public final String getPath() {
+        return this.path;
+    }
+
+}
