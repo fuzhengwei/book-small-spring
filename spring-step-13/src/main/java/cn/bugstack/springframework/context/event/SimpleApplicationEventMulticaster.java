f34d62e140d5353aa00aefa224ec6d93e454972e
小傅哥 <184172133@qq.com>
2022-10-30 16:28:05 +0800

    小傅哥，feat：第13章　自动扫描注册Bean对象

---
+++ spring-step-13/src/main/java/cn/bugstack/springframework/context/event/SimpleApplicationEventMulticaster.java
@@ -0,0 +1,28 @@
+package cn.bugstack.springframework.context.event;
+
+import cn.bugstack.springframework.beans.factory.BeanFactory;
+import cn.bugstack.springframework.context.ApplicationEvent;
+import cn.bugstack.springframework.context.ApplicationListener;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description Simple implementation of the {@link ApplicationEventMulticaster} interface.
+ * @date 2022/3/13
+ * @github https://github.com/fuzhengwei/CodeDesignTutorials
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class SimpleApplicationEventMulticaster extends AbstractApplicationEventMulticaster {
+
+    public SimpleApplicationEventMulticaster(BeanFactory beanFactory) {
+        setBeanFactory(beanFactory);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void multicastEvent(final ApplicationEvent event) {
+        for (final ApplicationListener listener : getApplicationListeners(event)) {
+            listener.onApplicationEvent(event);
+        }
+    }
+
+}
---
+++ spring-step-13/src/test/java/cn/bugstack/springframework/test/bean/UserService.java
@@ -0,0 +1,42 @@
+package cn.bugstack.springframework.test.bean;
+
+import cn.bugstack.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component("userService")
+public class UserService implements IUserService {
+
+    private String token;
+
+    public String queryUserInfo() {
+        try {
+            Thread.sleep(new Random(1).nextInt(100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "小傅哥，100001，深圳";
+    }
+
+    public String register(String userName) {
+        try {
+            Thread.sleep(new Random(1).nextInt(100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "注册用户：" + userName + " success！";
+    }
+
+    @Override
+    public String toString() {
+        return "UserService#token = { " + token + " }";
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
