1ec0400464aa0e045259f412e1398d72b6ab700b
小傅哥 <184172133@qq.com>
2022-10-30 16:23:48 +0800

    小傅哥，feat：第7章　Bean 对象的初始化和销毁

---
+++ spring-step-07/src/main/java/cn/bugstack/springframework/beans/factory/config/BeanDefinition.java
@@ -0,0 +1,64 @@
+package cn.bugstack.springframework.beans.factory.config;
+
+import cn.bugstack.springframework.beans.PropertyValues;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 定义 Bean 实例信息
+ * @date 2022/03/07
+ * @github https://github.com/fuzhengwei
+ * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class BeanDefinition {
+
+    private Class beanClass;
+
+    private PropertyValues propertyValues;
+
+    private String initMethodName;
+
+    private String destroyMethodName;
+
+    public BeanDefinition(Class beanClass) {
+        this.beanClass = beanClass;
+        this.propertyValues = new PropertyValues();
+    }
+
+    public BeanDefinition(Class beanClass, PropertyValues propertyValues) {
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues != null ? propertyValues : new PropertyValues();
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    public String getDestroyMethodName() {
+        return destroyMethodName;
+    }
+
+    public void setDestroyMethodName(String destroyMethodName) {
+        this.destroyMethodName = destroyMethodName;
+    }
+
+}
---
+++ spring-step-07/src/main/java/cn/bugstack/springframework/beans/factory/support/DefaultListableBeanFactory.java
@@ -0,0 +1,60 @@
+package cn.bugstack.springframework.beans.factory.support;
+
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.beans.factory.ConfigurableListableBeanFactory;
+import cn.bugstack.springframework.beans.factory.config.BeanDefinition;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 默认的Bean工厂实现类
+ * @date 2022/03/07
+ * @github https://github.com/fuzhengwei
+ * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements BeanDefinitionRegistry, ConfigurableListableBeanFactory {
+
+    private Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
+
+    @Override
+    public BeanDefinition getBeanDefinition(String beanName) {
+        BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
+        if (beanDefinition == null) throw new BeansException("No bean named '" + beanName + "' is defined");
+        return beanDefinition;
+    }
+
+    @Override
+    public void preInstantiateSingletons() throws BeansException {
+        beanDefinitionMap.keySet().forEach(this::getBean);
+    }
+
+    @Override
+    public void registerBeanDefinition(String beanName, BeanDefinition beanDefinition) {
+        beanDefinitionMap.put(beanName, beanDefinition);
+    }
+
+    @Override
+    public boolean containsBeanDefinition(String beanName) {
+        return beanDefinitionMap.containsKey(beanName);
+    }
+
+    @Override
+    public <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
+        Map<String, T> result = new HashMap<>();
+        beanDefinitionMap.forEach((beanName, beanDefinition) -> {
+            Class beanClass = beanDefinition.getBeanClass();
+            if (type.isAssignableFrom(beanClass)) {
+                result.put(beanName, (T) getBean(beanName));
+            }
+        });
+        return result;
+    }
+
+    @Override
+    public String[] getBeanDefinitionNames() {
+        return beanDefinitionMap.keySet().toArray(new String[0]);
+    }
+
+}
