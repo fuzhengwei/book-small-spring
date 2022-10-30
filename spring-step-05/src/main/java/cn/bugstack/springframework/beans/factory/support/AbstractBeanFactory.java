b5ae8021531a794552202b2b4f829a623a45234a
小傅哥 <184172133@qq.com>
2022-10-30 16:19:50 +0800

    小傅哥，feat：第5章　资源加载器解析文件注册对象

---
+++ spring-step-05/src/main/java/cn/bugstack/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -0,0 +1,45 @@
+package cn.bugstack.springframework.beans.factory.support;
+
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.beans.factory.BeanFactory;
+import cn.bugstack.springframework.beans.factory.config.BeanDefinition;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 抽象的 Bean 工厂基类，定义模板方法
+ * @date 2022/03/07
+ * @github https://github.com/fuzhengwei
+ * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
+
+    @Override
+    public Object getBean(String name) throws BeansException {
+        return doGetBean(name, null);
+    }
+
+    @Override
+    public Object getBean(String name, Object... args) throws BeansException {
+        return doGetBean(name, args);
+    }
+
+    @Override
+    public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
+        return (T) getBean(name);
+    }
+
+    protected <T> T doGetBean(final String name, final Object[] args) {
+        Object bean = getSingleton(name);
+        if (bean != null) {
+            return (T) bean;
+        }
+
+        BeanDefinition beanDefinition = getBeanDefinition(name);
+        return (T) createBean(name, beanDefinition, args);
+    }
+
+    protected abstract BeanDefinition getBeanDefinition(String beanName);
+
+    protected abstract Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args);
+
+}
---
+++ spring-step-05/src/test/java/cn/bugstack/springframework/test/bean/UserService.java
@@ -0,0 +1,35 @@
+package cn.bugstack.springframework.test.bean;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 模拟用户服务类
+ * @date 2022/03/09
+ * @github https://github.com/fuzhengwei
+ * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class UserService {
+
+    private String uId;
+
+    private UserDao userDao;
+
+    public String queryUserInfo() {
+        return userDao.queryUserName(uId);
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+}
