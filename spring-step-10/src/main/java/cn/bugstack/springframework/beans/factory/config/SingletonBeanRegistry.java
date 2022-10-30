872404cae214ff9c0657dd420f21279a299fa58f
小傅哥 <184172133@qq.com>
2022-10-30 16:26:07 +0800

    小傅哥，feat：第10章 容器事件和事件监听器

---
+++ spring-step-10/src/main/java/cn/bugstack/springframework/beans/factory/config/SingletonBeanRegistry.java
@@ -0,0 +1,26 @@
+package cn.bugstack.springframework.beans.factory.config;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 单例 Bean 注册表
+ * @date 2022/03/07
+ * @github https://github.com/fuzhengwei
+ * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public interface SingletonBeanRegistry {
+
+    /**
+     * 返回在给定名称下注册的（原始）单例对象。
+     * @param beanName 要查找的bean的名称
+     * @return 返回注册的单例对象
+     */
+    Object getSingleton(String beanName);
+
+    /**
+     * 注册单利对象
+     * @param beanName          Bean 对象名称
+     * @param singletonObject   Bean 对象
+     */
+    void registerSingleton(String beanName, Object singletonObject);
+
+}
---
+++ spring-step-10/src/test/java/cn/bugstack/springframework/test/ApiTest.java
@@ -0,0 +1,24 @@
+package cn.bugstack.springframework.test;
+
+import cn.bugstack.springframework.context.support.ClassPathXmlApplicationContext;
+import cn.bugstack.springframework.test.event.CustomEvent;
+import org.junit.Test;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 单元测试
+ * @date 2022/03/10
+ * @github https://github.com/fuzhengwei
+ * @copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class ApiTest {
+
+    @Test
+    public void test_event() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        applicationContext.publishEvent(new CustomEvent(applicationContext, 1019129009086763L, "成功了！"));
+
+        applicationContext.registerShutdownHook();
+    }
+
+}
