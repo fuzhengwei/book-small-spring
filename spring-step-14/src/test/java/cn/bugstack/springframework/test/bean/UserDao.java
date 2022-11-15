422c539bd3d1624e97fae8d05217861a29e0f30d
小傅哥 <fuzhengwei@users.noreply.github.com>
2022-11-15 14:04:26 +0800

    小傅哥，feat：第14章 通过注解注入属性信息

--- spring-step-14/src/test/java/cn/bugstack/springframework/test/bean/UserDao.java
+++ spring-step-14/src/test/java/cn/bugstack/springframework/test/bean/UserDao.java
@@ -14,6 +14,7 @@ public class UserDao {
         hashMap.put("10001", "小傅哥，北京，亦庄");
         hashMap.put("10002", "八杯水，上海，尖沙咀");
         hashMap.put("10003", "阿毛，香港，铜锣湾");
+        hashMap.put("10004", "花花，天津，东丽");
     }
 
     public String queryUserName(String uId) {
