cd6cf6526d50d49dcc7e29853bba90e148ecc372
小傅哥 <184172133@qq.com>
2022-11-21 15:22:15 +0800

    小傅哥，feat：第2章 实现Bean对象的定义、注册和获取

--- spring-step-02/src/test/java/cn/bugstack/springframework/test/ApiTest.java
+++ spring-step-02/src/test/java/cn/bugstack/springframework/test/ApiTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 
 /**
  * @author 小傅哥，微信：fustack
+ *
+ * 来自于对开源项目的学习；
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  * @description 测试类
  * @date 2022/03/07
  * @github https://github.com/fuzhengwei
